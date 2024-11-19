e6edb45c904fefc14513b2223085d29ab3191a1f
Estlib <75094683+Estlib@users.noreply.github.com>
2024-11-19 12:35:19 +0200

    Project Setup - #10 The project Backend has a /docs->

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const app = require('express')();
 const port = 8080;
+const app = require('express')();
+const swaggerUI = require('swagger-ui-express');
+const swaggerDoc = require("./docs/swagger.json");
 
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.listen(port, () => {console.log(`Api on saadaval aadressil: http://localhost:${port}`);});
