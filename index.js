d7d2a886348549159a4c642ec57ac2a847267f28
Estlib <75094683+Estlib@users.noreply.github.com>
2024-11-19 13:15:12 +0200

    app.get /games. documented enpoint, first method written

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const swaggerUI = require('swagger-ui-express');
 const swaggerDoc = require("./docs/swagger.json");
 
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+
+app.get("/games", (req, res) => { res.send( ["Throne & Liberty","Goat Simulator","Team Fortress 2"]
+    /*[
+     { GameID: 1, GameName: "Throne & Liberty", ReleaseDateEU: "01.10.2024", ReviewScore: 10 },
+    { GameID: 2, GameName: "Goat Simulator", ReleaseDateEU: "01.04.2014", ReviewScore: 10 },
+    { GameID: 3, GameName: "Team Fortress 2", ReleaseDateEU: "01.01.2007", ReviewScore: 10 }
+] */)})
+
+
+
 app.listen(port, () => {console.log(`Api on saadaval aadressil: http://localhost:${port}`);});
